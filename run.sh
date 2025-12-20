81115b57d4556d8c1694c6431ed8b43b177a3847
Ansh Dadwal <anshdadwal298@gmail.com>
2025-12-20 13:22:59 +0530

    use kivy develop

--- run.sh
+++ run.sh
@@ -31,7 +31,7 @@ export APP_ANDROID_ACCEPT_SDK_LICENSE=1
 export BUILDOZER_WARN_ON_ROOT=0
 
 pip3 install --user --upgrade buildozer cython virtualenv
-pip3 install git+https://github.com/T-Dynamos/python-for-android@recipe_fix
+pip3 install git+https://github.com/kivy/python-for-android@develop
 
 # https://github.com/kivy/python-for-android@develop
 
